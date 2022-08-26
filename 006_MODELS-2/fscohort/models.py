3e67d1df0b00bbc591ecf4748e3f20b63daa0a14
halil <halilerk77@icloud.com>
2022-08-26 20:51:03 +0300

    .

--- 006_MODELS-2/fscohort/models.py
+++ 006_MODELS-2/fscohort/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Student(models.Model):
+    first_name = models.CharField("Adı",max_length=50)
+    last_name = models.CharField("Soyadı",max_length=50)
+    number = models.IntegerField("Numara")
+    
+    
+    def __str__(self):
+        return f"{self.number}-{self.first_name} {self.last_name}"
+    
+    class Meta:
+        ordering = ["number"]  # "-number" yaparsan tersten sıralar
+        verbose_name_plural = "Öğrenciler"
