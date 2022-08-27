f15487e909595fce5bfd623ec9162443b4c2b065
halil <halilerk77@icloud.com>
2022-08-27 16:32:50 +0300

    .

--- 006_MODELS-2/relations/admin.py
+++ 006_MODELS-2/relations/admin.py
@@ -1,5 +1,8 @@
+from tkinter import Frame
 from django.contrib import admin
 
 from .models import *
 admin.site.register(Creator)
 admin.site.register(Language)
+admin.site.register(Framework)
+admin.site.register(Developer)
--- 006_MODELS-2/relations/models.py
+++ 006_MODELS-2/relations/models.py
@@ -32,3 +32,28 @@ on_delete properties:
     # DO_NOTHING -> if primary deleted, do nothing.
     # PROTECT -> if foreign is exist, can not delete primary.
 '''
+
+
+
+# many2one relation:
+class Framework(models.Model):
+    name = models.CharField(max_length=50)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} - {self.language}"
+
+    class Meta:
+        ordering = ['name']
+
+# many2many relation:
+class Developer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    framework = models.ManyToManyField(Framework) # Not use on_delete!
+
+    def __str__(self):
+        return f"{self.first_name} - {self.last_name}"
+
+    class Meta:
+        ordering = ['first_name']
