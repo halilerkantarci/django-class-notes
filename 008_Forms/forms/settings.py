ad71b4d74036b550b2155ab961dc45784c765c9a
halil <halilerk77@icloud.com>
2022-08-30 00:01:14 +0300

    .

--- 008_Forms/forms/settings.py
+++ 008_Forms/forms/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "student"
 ]
 
 MIDDLEWARE = [
@@ -112,6 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
