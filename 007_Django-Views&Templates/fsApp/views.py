9ad42434beaaf7170a896d2d3635895f3989c181
halil <halilerk77@icloud.com>
2022-08-29 18:28:25 +0300

    TEMPLATE

--- 007_Django-Views&Templates/fsApp/views.py
+++ 007_Django-Views&Templates/fsApp/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-def home(request):
-    print(request)
-    print(request.method)
-    html = "<html><body><h1>hello from the other side.</h1></body></html>"
-    return HttpResponse(html)
+# def home(request):
+#     print(request)
+#     print(request.method)
+#     print(request.COOKIES)
+#     print(request.path)
+#     print(request.META)
+#     {{ request.user }}
+#     # print(request.user)
+#     html = "<html><body><h1>hello from the other side.</h1></body></html>"
+#     return HttpResponse(html)
 
+
+def home(request):
+    return render(request,"fsApp/index.html")
--- 007_Django-Views&Templates/prj/urls.py
+++ 007_Django-Views&Templates/prj/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("fsApp.urls")),
+    path("fs", include("fsApp.urls")),
+    path("ds", include("dsApp.urls")),
 ]
