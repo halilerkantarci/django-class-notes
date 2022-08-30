0acb6556d216293f89888c79c0ea0609c0937453
halil <halilerk77@icloud.com>
2022-08-31 00:07:32 +0300

    form with django

--- 008_Forms/student/views.py
+++ 008_Forms/student/views.py
@@ -1,7 +1,18 @@
+from multiprocessing import context
 from django.shortcuts import render
+from .forms import StudentForm
 
 def index(request):
     return render(request, 'student/index.html')
 
+# def student_page(request):
+#     return render(request,'student/student.html')
+
 def student_page(request):
-    return render(request,'student/student.html')
+    print(request.POST)
+    print(request.FILES)
+    form = StudentForm()
+    context = {
+        "form" : form
+    }
+    return render(request,"student/student.html",context)
