2aae6589c9144cd6961057d0a18d96505d2cd558
halil <halilerk77@icloud.com>
2022-08-31 01:59:11 +0300

    forms

--- 008_Forms/student/views.py
+++ 008_Forms/student/views.py
@@ -1,6 +1,8 @@
 from multiprocessing import context
 from django.shortcuts import render
 from .forms import StudentForm
+from student.models import Student
+
 
 def index(request):
     return render(request, 'student/index.html')
@@ -8,11 +10,29 @@ def index(request):
 # def student_page(request):
 #     return render(request,'student/student.html')
 
+# def student_page(request):
+#     print(request.POST)
+#     print(request.FILES)
+#     form = StudentForm()
+#     context = {
+#         "form" : form
+#     }
+#     return render(request,"student/student.html",context)
+
+
 def student_page(request):
-    print(request.POST)
-    print(request.FILES)
-    form = StudentForm()
-    context = {
-        "form" : form
-    }
-    return render(request,"student/student.html",context)
+    form = StudentForm
+    if request.method == "POST":
+        form = StudentForm(request.POST,request.FILES)
+        if form.is_valid():
+            student_data = {
+                "first_name":form.cleaned_data.get("first_name"),
+                "last_name":form.cleaned_data.get("last_name"),
+                "number":form.cleaned_data.get("number"),
+                "profile_pic":form.cleaned_data.get("profile_pic")
+            }
+            student = Student(**student_data) # Student.objects.create(**student_data)   ((bu şekilde yazarsan student.save() yazmana gerek yok))
+            student.save()
+        context = {
+            "form" : form
+        }
