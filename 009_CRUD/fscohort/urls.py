64cbc68b517283ef95daa08705307fc7d6468e97
halil <halilerk77@icloud.com>
2022-08-31 18:59:01 +0300

    crud-read

--- 009_CRUD/fscohort/urls.py
+++ 009_CRUD/fscohort/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index,student_list
 
 urlpatterns = [
     path("", index,name="index"),
+    path('list/',student_list , name='list'),
 ]
