678c11d892607228794a9930c0af4fbeada1d7ad
halil <halilerk77@icloud.com>
2022-09-05 19:19:02 +0300

    add user

--- 012_DJ_Auth_1/user_example/urls.py
+++ 012_DJ_Auth_1/user_example/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from .views import (
     home,
-    special
+    special,
+    register
 )
 
 urlpatterns = [
     path('', home, name="home"),
+    path('register/', register, name='register'),
     path('special/', special, name="special"),
 ]
