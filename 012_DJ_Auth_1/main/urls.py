d696d0e31d0118183134bdc62014520cb287fe24
halil <halilerk77@icloud.com>
2022-09-12 18:53:43 +0300

    password change

--- 012_DJ_Auth_1/main/urls.py
+++ 012_DJ_Auth_1/main/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_example.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name="registration/password_change.html"), name="password_change")
 ]
